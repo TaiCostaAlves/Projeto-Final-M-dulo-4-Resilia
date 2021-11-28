449ab627968cb69c4850fbc2c00c74809fefb8e7
tainaracosta92@hotmail.com <tainaracosta92@hotmail.com>
2021-11-28 12:03:17 -0300

    Instalando Cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const customExpress = require("./config/customExpress")
 const app = customExpress ()
 const PORT = process.env.PORT || 3000
-const Menu  = require('./controller/Menu')
-const bdVerdadeiro = require('./infra/sqlite-db');
+//const Menu  = require('./controller/Menu')
+// app.get('/',(req, res) => {
+//     res.send ({
+//         'Oi': 'Oi'
+//     })
+// })
 
 app.listen(PORT, () =>{
     console.log("Servidor rodando na porta 3000")
--- src/controllers/menu..js
+++ src/controllers/menu..js
@@ -1,24 +1,34 @@
 const Menu = require("../models/Menu")
-// module.exports = (app) => {
-//     app.get("/", (req, res) => {
-//         return res.send("<h1>Funciona</h1>")
+const bdCaixa = require('../infra/sqlite-db');
+module.exports = (app) => {
+    app.get("/", (req, res) => {
+      console.log('Rodando')
+      bdCaixa.all('SELECT * FROM MENU', (err, linhas)=>{
+                if(err){
+                  res.json({"menssagem": err.message, "erro": true})
+                } else {
+                 
+                  res.json({"caixa": linhas, "count": linhas.legth, "erro": false})
+                }
+              })
 
-//     })
-//     app.get("/menu", (req,res)=>{
-//         Menu.busca(res)
-//     }) 
-// }
-const Menu = (app , bdVerdadeiro) => { 
-    app.get('/Menu', (req, res) => {
-      bdVerdadeiro.all('SELECT * FROM MENU', (err, linhas)=>{
-        if(err){
-          res.json({"menssagem": err.message, "erro": true})
-        } else {
-          res.json({"caixa": linhas, "count": linhas.legth, "erro": false})
-        }
-      })
-     
     })
-} 
+    app.get("/menu", (req,res)=>{
+        Menu.busca(res)
+    }) 
+}
 
-module.exports  =  caixa
+// const Menu = (app , bdVerdadeiro) => { 
+//     app.get('/Menu', (req, res) => {
+//       bdVerdadeiro.all('SELECT * FROM MENU', (err, linhas)=>{
+//         if(err){
+//           res.json({"menssagem": err.message, "erro": true})
+//         } else {
+//           res.json({"caixa": linhas, "count": linhas.legth, "erro": false})
+//         }
+//       })
+     
+//     })
+// } 
+
+// module.exports  =  caixa
